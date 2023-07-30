bda29398cb6ccd374b96e6c55a4cf9595f3cdbc0
Max Brauer <mbrauer@vmware.com>
2023-07-30 14:01:42 +0200

    depend on k8s.io things
    Signed-off-by: Max Brauer <mbrauer@vmware.com>

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
-
 package main
 
 import (
 	"log"
 
 	stkv1alpha1 "gitlab.eng.vmware.com/services-control-plane/resource-claims/api/services/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
 )
 
 func main() {
-  log.Printf("%#v", stkv1alpha1.ResourceClaim{})
-  log.Print("ok")
+	log.Printf("%#v", stkv1alpha1.ResourceClaim{})
+	log.Printf("%#v", corev1.Secret{})
+	log.Print("ok")
 }
